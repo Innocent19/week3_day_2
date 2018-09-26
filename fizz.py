85b9220ff26db4ca22f1f21a6251fd5ef46755f4
Innocent19 <innocentakampurira57@gmail.com>
2018-09-26 19:11:06 +0300

    added day 2 challenges

--- fizz.py
+++ fizz.py
@@ -1,9 +1,13 @@
-def Fizzbuzz(list1,list2):
-    if (len(list1)+ len(list2))%3 is 0 and (len(list1)+ len(list2))%5 is not 0:
-        return "Fizz"
-    elif (len(list1)+ len(list2))%5 is 0 and (len(list1)+ len(list2))%3 is not 0:
-        return "Buzz" 
-    while (len(list1)+ len(list2))%3 is 0 and (len(list1)+ len(list2))%5 is 0:
-        return "Fizzbuzz"  
-
-print (Fizzbuzz ("apples","banana"))
+def fizzbuzz(list1,list2
+):
+    # try:
+        if (len(list1)+ len(list2))%3 == 0 and (len(list1)+ len(list2))%5 != 0:
+            return "Fizz"
+        elif (len(list1)+ len(list2))%5 == 0 and (len(list1)+ len(list2))%3 != 0:
+            return "Buzz" 
+        while (len(list1)+ len(list2))%3 == 0 and (len(list1)+ len(list2))%5 == 0:
+            return "Fizzbuzz" 
+    # except TypeError:
+x = input("Enter list1 : ")
+y = input("Enter list2 : ")
+print (fizzbuzz (x,y))
--- funct.py
+++ funct.py
@@ -1,3 +1,16 @@
-def check_vow   
-    
+def function_takes_string():
+    vowels = "aeiou"
+    new_string = str()
+    input_string= input("Enter your string of choice:")
+    no_duplications = set(input_string)
+    duplicates_length = len(input_string) - len(no_duplications)
+    for letter in input_string:
+         if letter in vowels:
+             new_string+=letter
+
+    print((new_string, duplicates_length)) 
+function_takes_string()    
+
+
+
 
